c98c4d58d974ce728e51354a8946bd08b58620fc
xiaowt1205 <41275478+xiaowt1205@users.noreply.github.com>
2020-02-14 15:26:53 +0800

    add InsidePagetemplate

--- js/index.js
+++ js/index.js
@@ -1,18 +1,3 @@
-$('.menu-btn').click(function () {
-    $('.menu-btn').toggleClass('active');
-    // $('.menu').toggleClass('active');
-    if ($('.menu').hasClass('active')) {
-        $('.menu').addClass('fadeout')
-        setTimeout(
-            function () {
-                $('.menu').removeClass('active')
-                $('.menu').removeClass('fadeout')
-            }, 600
-        )
-    } else {
-        $('.menu').addClass('active')
-    }
-})
 
 var swiper = new Swiper('.swiper-banner', {
     pagination: {
@@ -20,3 +5,4 @@ var swiper = new Swiper('.swiper-banner', {
         clickable: true,
     },
 });
+
