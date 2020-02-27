6cd11448654e15627df186ef1cb6c418fde2924e
Nero03026 <41275554+Nero03026@users.noreply.github.com>
2020-02-27 22:59:11 +0800

    彰祐-首頁調整

--- js/index.js
+++ js/index.js
@@ -1,23 +1,36 @@
-var swiper = new Swiper('.swiper-banner', {
+var swiper = new Swiper('.banner .swiper-banner', {
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
     },
 });
 
-
+var swiper = new Swiper('.vendor_area .swiper-container', {
+    slidesPerView: 2,
+    spaceBetween: 30,
+    breakpoints: {
+        // when window width is >= 1200px
+        768: {
+            slidesPerView: 4,
+        },
+    },
+    navigation: {
+        nextEl: '.vendor_area .arrow_button-next',
+        //prevEl: '.picture_area .arrow_button-prev',
+    },
+});
 
 var doOnce = false;
 
 function dynamicNumberCounter(scrollPos, id, setNum) {
     var inner = document.getElementById(id);
-    
+
     var num = 0;
     var set = null;
     var AddNumdelay = 50;
     var setNum = setNum;
 
-    if ((document.body.scrollTop >= scrollPos && document.body.scrollTop <= scrollPos+150) || (document.documentElement.scrollTop >= scrollPos && document.documentElement.scrollTop <= scrollPos+150)) {
+    if ((document.body.scrollTop >= scrollPos && document.body.scrollTop <= scrollPos + 150) || (document.documentElement.scrollTop >= scrollPos && document.documentElement.scrollTop <= scrollPos + 150)) {
         if (doOnce === false) {
             inner.innerHTML = 0
             set = setInterval(function () {
