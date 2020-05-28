a83e1f9aab9a2db306106a81d9222773b9f486d0
xiaowt1205 <41275478+xiaowt1205@users.noreply.github.com>
2020-05-28 14:15:12 +0800

    上傳檔案 新增上一次上傳的檔案提示

--- js/Application_input.js
+++ js/Application_input.js
@@ -1,12 +1,30 @@
-var files = document.querySelectorAll('.fileInput')
+var files = document.querySelectorAll('.files')
+var file_ipt = document.querySelectorAll('.fileInput')
+var lastFile = document.querySelectorAll('.lastFile')
 var fileNames = document.querySelectorAll('.fileName')
 
 
-files.forEach(function(element,index){
-    element.onchange = function(){
-        var iptVal = element.value
+file_ipt.forEach(function (element, index) {
+    element.onclick = function () {
+        let nowVal = element.value
+        if (nowVal == "") {
+
+        } else {
+            lastFile[index].classList.add('active')
+            lastFile[index].innerHTML = `<div>${nowVal.split("\\").pop()}<div class="delete" onclick="remove(this)"></div></div>`
+        }
+    }
+    element.onchange = function () {
+        let iptVal = element.value
         fileNames[index].innerHTML = iptVal
     }
 });
 
 
+function remove(el) {
+    var li = el.parentNode.parentNode;
+    var div = el.parentNode;
+    // console.log(element)
+    li.classList.remove('active')
+    div.remove();
+}
